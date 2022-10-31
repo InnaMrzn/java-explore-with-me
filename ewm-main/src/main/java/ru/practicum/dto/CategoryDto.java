b7b821614d1b0a4652019489ba039663b1dd382a
unknown <inna_murzin@yahoo.com>
2022-10-31 11:56:17 +0300

    fix: исправить ошибки стиля

--- ewm-main/src/main/java/ru/practicum/dto/CategoryDto.java
+++ ewm-main/src/main/java/ru/practicum/dto/CategoryDto.java
@@ -1,10 +1,6 @@
 package ru.practicum.dto;
 
 import lombok.Value;
-
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
-
 @Value
 public class CategoryDto {
     Long id;
