455e2fb20a1473d69e9f91d140410da3e7ea85af
unknown <inna_murzin@yahoo.com>
2022-11-08 11:06:57 +0300

    fix: исправить форматирование

--- ewm-main/src/main/java/ru/practicum/dto/NewCompilationDto.java
+++ ewm-main/src/main/java/ru/practicum/dto/NewCompilationDto.java
@@ -1,9 +1,7 @@
 package ru.practicum.dto;
 
 import lombok.Data;
-
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.List;
 
